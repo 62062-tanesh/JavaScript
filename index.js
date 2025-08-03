8e8f86454cfedb8b99581e9ecdf992764ffd76ce
62062-tanesh <tanesh8409@gmail.com>
2025-08-03 23:27:01 +0530

    Added JavaScript Learning

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log(2+2);
+// alert("THis is my web page");
+// document.querySelector('#OnlyNew'onclick ="(alert('I am clicked'))";
+console.log("Hello");
+console.warn('This is a Warning');
