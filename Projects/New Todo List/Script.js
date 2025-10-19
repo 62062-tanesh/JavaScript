94a34f0ef94a82c60f0c764b76848b6eb324e314
62062-tanesh <tanesh8409@gmail.com>
2025-10-20 00:06:05 +0530

    Update

--- Projects/New Todo List/Script.js
+++ Projects/New Todo List/Script.js
@@ -10,17 +10,46 @@ function saveTodos(){
 
 }
 function createTodoNode(todo, index){
-    const li = document.createElement('input');
+    const li = document.createElement('li');
+    const checkbox =  document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = !!todo.completed;
     checkbox.addEventListener("change",()=>{
         todo.completed = checkbox.checkbox;
+        textSpan.style.textDecoration = todo.completed ? 'line-through':"";
 
 
         saveTodos();  
     })
 
-    
+    const textSpan = document.createElement("span");
+    textSpan.textContent = todo.text;
+    textSpan.style.margin = '0 8px';
+    if(todo.completed){
+        textSpan.style.textDecoration = 'line-through';
+    }
+        document.addEventListener("dblclick", () => {
+            const newText = prompt("Edit todo", todo.text);
+            if(newText !== null){
+                todo.text = newText.trim()
+                textSpan.textContent = todo.text;
+                saveTodos();
+            }
+        })
+
+        const delBtn = document.createElement('button');
+        delBtn.textContent = "Delete";
+        delBtn.addEventListener('click', () => {
+            todos.splice(index,1);
+            render();
+            saveTodos();
+        })
+
+        li.appendChild(checkbox);
+        li.appendChild(textSpan);
+        li.appendChild(delBtn);
+        return li;
+
 }
 
 function render(){
@@ -30,3 +59,18 @@ function render(){
         list.appendChild(node)
     });
 }
+
+function addTodo(){
+    const text = input.value.trim();
+    if(!text){
+        return;
+    }
+
+    todos.push({text, completed: false});
+    input.value = '';
+    render();
+    saveTodos();
+}
+
+addBtn.addEventListener("click", addTodo);
+render();
