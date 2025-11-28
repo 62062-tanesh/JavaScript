718d3ed26ccff186e7bac4e545342abed4e434f6
62062-tanesh <tanesh8409@gmail.com>
2025-11-29 01:11:03 +0530

    Commit

--- Practice Question/app.js
+++ Practice Question/app.js
@@ -1,4 +1,26 @@
-function sum(a,b){
-    console.log(a+b);
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+
+// function calculator(a,b, sumCallback){
+//     sumCallback(a,b);
+// }
+
+// calculator(5,6,sum);
+
+function getData(dataId, getNextData){
+    setTimeout(()=>{
+        console.log("data:",dataId);
+        if(getNextData){
+            getNextData();
+        }
+    }, 2000);
 }
-sum(5,6);
+
+getData(1, ()=>{
+    getData(2);
+});
+
+
+
