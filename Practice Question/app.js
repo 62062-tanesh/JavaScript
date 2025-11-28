3175fd1848ac3388284792f452406f2b79541185
62062-tanesh <tanesh8409@gmail.com>
2025-11-29 02:00:38 +0530

    Practicing JS Promise Concept

--- Practice Question/app.js
+++ Practice Question/app.js
@@ -9,18 +9,50 @@
 
 // calculator(5,6,sum);
 
-function getData(dataId, getNextData){
-    setTimeout(()=>{
-        console.log("data:",dataId);
-        if(getNextData){
-            getNextData();
-        }
-    }, 2000);
+// function getData(dataId, getNextData){
+//     setTimeout(()=>{
+//         console.log("data:",dataId);
+//         if(getNextData){
+//             getNextData();
+//         }
+//     }, 2000);
+// }
+
+// getData(1, ()=>{
+//     getData(2);
+// });
+
+
+// let promise = new Promise((resolve,reject) => {
+//     console.log("This is a Promise.");
+//     resolve("Something Error")
+// })
+// console.log("Hello");
+
+
+// function getData(dataId, getNextData){
+//     return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log("data:",dataId);
+//         if(getNextData){
+//             getNextData();
+//         }
+//     }, 2000);
+//     })
+// }
+
+// getData(1, ()=>{
+//     getData(2);
+// });
+
+
+function asyncFunc(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{console.log("Data1"); resolve("success")},4000)
+    })
 }
-
-getData(1, ()=>{
-    getData(2);
-});
-
-
-
+console.log("Fetching Data1.........");
+let p1 = asyncFunc();
+p1.then((res)=>{
+    console.log("Hello",res);
+})
